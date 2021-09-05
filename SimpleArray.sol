7fa5d242d41b7754c9f923433d5fb13595acbd9f
Dustin Gearhart <gearhart26@gmail.com>
2021-09-05 03:41:34 -0700

    Update SimpleArray.sol

--- SimpleArray.sol
+++ SimpleArray.sol
@@ -31,5 +31,14 @@ contract simpleList {
   function getEntityCount() public view returns(uint entityCount) {
     return entityStructs.length;
   }
-
 }
+
+// Array Drawbacks: slow, expensive, very complicated to delete values, takes duplicate data, does not scale well, slows down more and more as entities are added, increased function complexity
+
+// Array Benifits: can iterate through, can look up number of entries, simple, everything saved in cronological order, can access entities by ID, can run analitics on all entities saved in array
+
+
+// Extra Note: 
+// Arrays do not have an initial state like mappings do. This makes deleting or removing data very complicated because you cannot delete a value in the middle of an array without replacing it. 
+// You can only delete the first or last values in an array and there is no intial state to revert it to so you can only really add a isDeleted boolian value for the struct like we use the isEntity boolian in simple mapping.
+// However, this does not work as well with arrays since arrays slow down with each new value saved. This means you do not want to have anything saved there that is not absolutly necessary
