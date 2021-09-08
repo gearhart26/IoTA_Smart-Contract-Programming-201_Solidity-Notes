dfe41a8b61665067bfe2af036cf74313c48112f4
Dustin Gearhart <gearhart26@gmail.com>
2021-09-08 00:18:33 -0700

    Update Mapping_w_Index_and_Delete.sol

--- Mapping_w_Index_and_Delete.sol
+++ Mapping_w_Index_and_Delete.sol
@@ -1,1 +1,6 @@
+// topic = STORAGE DESIGN PATTERNS  -->  video = FINAL SOLUTION //
 
+//This is the best solution to the storage structure drawbacks for now. 
+//This is the storage structure that is commonly used in current smart contracts.
+
+pragma solidity 0.8.0;
