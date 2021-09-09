b9c2b614e0a035c16cea1f470a097737b26d275c
Dustin Gearhart <gearhart26@gmail.com>
2021-09-09 01:36:30 -0700

    Update Re-EntrencyProtection.sol

--- Re-EntrencyProtection.sol
+++ Re-EntrencyProtection.sol
@@ -1,1 +1,8 @@
+// topic = Security Considerations --> video = RE-ENTRENCY //
 
+// Checks --> Effects (changing state) --> Interatctions
+    //this pattern always needs to be followed to prevent re-entrency attacks 
+    
+//.call used in test_3 contract is currently the best way to send ETH but it has to be used in conjunction with the checks, effects, iinteractions security pattern
+
+pragma solidity 0.8.0;
